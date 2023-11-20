50c1c44bbb3c8570670d084cc32f0e82cea31ce2
amandagteo05 <148063834+amandagteo05@users.noreply.github.com>
2023-11-20 10:13:42 -0300

    .

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -8,7 +8,7 @@ const listadeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0,
 
-while(contador<listadeTeclas.lenght){
+for(contador<listadeTeclas.lenght){
 
     const tecla = listadeTeclas[contador]
 
@@ -23,3 +23,33 @@ while(contador<listadeTeclas.lenght){
     contador = contador + 1;
    
 }
+
+ function tocasom(idElemento){
+    document,querySelector(idElementoAudio),play()
+ }
+
+ consta listadeTeclas = documentSelectorAll('.tecla');
+
+ let contador = 0;
+
+  for (let contador = 0; contador < listadeTeclas.lenght; contador ++>){
+
+    const tecla listadeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+  
+  tecla.onkeydown = function (evento){
+
+    if(evento.code === 'Enter'|| evento.code==='Space')
+    tecla.classList.add('ativa');
+}
+
+tecla.onleyup = function () {
+    tecla.classList.remove('ativa');
+}
+  }
+ 
